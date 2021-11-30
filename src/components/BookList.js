f76eff87332ef62346495690c4aaa4e9276b9dd3
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2021-11-30 15:23:07 -0300

    Consuming Context with function components

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,27 +1,23 @@
-import { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends Component{
-
-    render(){
-        return(
-            <ThemeContext.Consumer>
-                {(context)=>{
-                    const {isLightTheme, light, dark} = context;
-                    const theme = isLightTheme? light : dark;
-                    return (
-                        <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
-                            <ul>
-                                <li style={{background: theme.ui}}>The way of kings</li>
-                                <li style={{background: theme.ui}}>The name of the wind</li>
-                                <li style={{background: theme.ui}}>The final empire</li>
-                            </ul>
-                        </div>
-                    )
-                }}
-            </ThemeContext.Consumer>
-        );
-    }
+const BookList = () => {
+    return(
+        <ThemeContext.Consumer>
+            {(context)=>{
+                const {isLightTheme, light, dark} = context;
+                const theme = isLightTheme? light : dark;
+                return (
+                    <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
+                        <ul>
+                            <li style={{background: theme.ui}}>The way of kings</li>
+                            <li style={{background: theme.ui}}>The name of the wind</li>
+                            <li style={{background: theme.ui}}>The final empire</li>
+                        </ul>
+                    </div>
+                )
+            }}
+        </ThemeContext.Consumer>
+    );
 }
 
 export default BookList;
