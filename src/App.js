c192892b8148b5ec7672f486f71f273bb370aa34
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2021-11-30 15:43:25 -0300

    Creating Multiple Contexts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import BookList from "./components/BookList";
 import Navbar from "./components/NavBar";
 import ThemeToggle from "./components/ThemeToogle";
+import AuthContextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar></Navbar>
-        <BookList></BookList>
-        <ThemeToggle></ThemeToggle>
+        <AuthContextProvider>
+          <Navbar></Navbar>
+          <BookList></BookList>
+          <ThemeToggle></ThemeToggle>
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
